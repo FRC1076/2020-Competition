445c839db4aea4499935abee4c65a0d37ff5ebd4
Soren N-T <sorenfnt@gmail.com>
2020-01-24 16:52:28 -0800

    added new test image and added video capture function

--- imagetest.py
+++ imagetest.py
@@ -26,18 +26,21 @@ def findObjectContours(dilate, objName):
     dilcopy = dilate.copy()
     contours, hierarchy = cv2.findContours(dilate, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     con = contours[0]
-    x, y , w, h = cv2.boundingRect(con)
-    center = (int(x+(w/2)), int(y+(h/2)))
+    x, y, w, h = cv2.boundingRect(con)
+    obj_center = (int(x+(w/2)), int(y+(h/2))) 
+    img_center = (dilate.shape[1]/2, dilate.shape[0]/2)
+    xydist = (obj_center[0]-img_center[0], obj_center[1] -img_center[1])
+    print(str(xydist) + "x,y distances")
     #moments = cv2.moments(con)
     #print(moments)
-    print(len(contours))
+    print(str(len(contours)) + " -contour length")
     #print(contours)
     contoured_image = cv2.drawContours(dilcopy, contours, -1, (100,0,0), 2)
     contoured_image = cv2.rectangle(contoured_image, (x,y), (x+w, y+h), (100, 0, 0), 2)
-    contoured_image = cv2.circle(contoured_image, center, 3, (100, 0, 0), 2)
+    contoured_image = cv2.circle(contoured_image, obj_center, 3, (100, 0, 0), 2)
     cv2.imshow("contours", contoured_image)
     cv2.waitKey(0) #Waits long enough for image to load
-
+    
     # # Only proceed if contours were found
     # if(contours != None):
     #     if(len(contours) > 1):
@@ -58,34 +61,41 @@ def findObjectContours(dilate, objName):
     #         for contour_boundary in contour_boundaries[:-1]:
     #             displayObject(contour_boundary, objName)
     #         return displayObject(contour_boundaries[-1], objName)
-
+def capture_images(device):
+    webcam = cv2.VideoCapture(device)
+    _, frame = webcam.read()
+    cv2.imshow("frame", frame)
+    cv2.waitKey(0)
 if __name__ == "__main__":
     print("Imagetest main is running")
-    bgr_img = cv2.imread("test_images/whiteboard_hex.png")
+    #bgr_img = cv2.imread("test_images/Green_Trapezoid.jpg")
     # cv2.imshow("Hello", bgr_img)
     # cv2.waitKey(5000) # keeps the window open for 5 seconds--long enough to load the image
-    # # Convert the frame to HSV
-    hsv_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2HSV)
-    # # cv2.imshow("Win1", bgr_img)
+    # Convert the frame to HSV
+    #hsv_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2HSV)
+    # cv2.imshow("Win1", bgr_img)
 
-    # # Find the cube
+    # Find the cube
     #img_hsv_lower = np.array([70, 170, 30])
     #img_hsv_upper = np.array([90, 200, 50])
-    color = whiteboard_color = np.array([110,91,80])
+    #color = whiteboard_color = np.array([110,91,80])
     #color = green_color = np.array([34, 177, 76])
-    bound = 40
-    img_hsv_lower = np.array(color - bound)
-    img_hsv_upper = np.array(color + bound)
-    img_dilate = removeNoise(bgr_img, (5,5), img_hsv_lower, img_hsv_upper)
-    cv2.imshow("dilated image", img_dilate)
-    cv2.waitKey(0)
+    #bound = 40
+    #img_hsv_lower = np.array(color - bound)
+    #img_hsv_upper = np.array(color + bound)
+    #img_hsv_lower = np.array([50, 170, 130])
+    #img_hsv_upper = np.array([180, 255, 225])
+    #img_dilate = removeNoise(bgr_img, (5,5), img_hsv_lower, img_hsv_upper)
+    #cv2.imshow("dilated image", img_dilate)
+    #cv2.waitKey(0)
     #print (hex_dilate.shape)
     #hex_img = np.array([])
-    findObjectContours(img_dilate, "retroreflective")
+    #findObjectContours(img_dilate, "retroreflective")
+    capture_images(10)
     while True:
 
         continue
-        
+
     # # # Display the BGR image with found objects bounded by rectangles
     # # if(displayImages):
     # #     cv2.imshow("Objects found!", bgr_img)
