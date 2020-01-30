0e92ccd7db0cfb7bb781ff686755c6f94a255775
William Ballbach <2021ballbachwilliamd@aaps.k12.mi.us>
2020-01-29 19:09:53 -0500

    Made robot.py (hopefully) driveable

--- robot.py
+++ robot.py
@@ -1,34 +1,53 @@
-import math
-import ctre 
 import wpilib
+from wpilib.interfaces import GenericHID
 
-import robotpy_ext.common_drivers
+#This year, all IDs are stored in the robotmap
+import robotmap
 
-PORT = 1
-
+LEFT_HAND = GenericHID.Hand.kLeft
+RIGHT_HAND = GenericHID.Hand.kRight
 
 class Robot(wpilib.TimedRobot):
     def robotInit(self):
         #DRIVETRAIN
-        print("It's Alive")
-        self.stick = wpilib.XboxController(0)
-        self.motor1 = ctre.WPI_TalonSRX(PORT)
+        self.driver = wpilib.XboxController(0)
+        self.operator = wpilib.XboxController(1)
+
+        self.left_side = createTalonAndFollower(robotmap.robot["LEFT_MASTER_ID"], robotmap.robot["LEFT_FOLLOWER_ID"])
+        self.right_side = createTalonAndFollower(robotmap.robot["RIGHT_MASTER_ID"], robotmap.robot["RIGHT_FOLLOWER_ID"])
+
+        self.drivetrain = wpilib.drive.DifferentialDrive(self.left_side, self.right_side)
 
     def robotPeriodic(self):
         return
+    
+    def autonomousInit(self):
+        pass
+
+    def autonomousPeriodic(self):
+        pass
 
     def teleopInit(self):
-        print("TELEOP BEGINS")
-        #self.motor2 = ctre.WPI_TalonSRX(PORT)
-       # self.drive = DifferentialDrive(self.motor1, self.motor2)
         self.encoder = wpilib.Encoder(0,1)
         # setup wheel diameter
+        print("Teleop begins!")
 
     def teleopPeriodic(self):
-        forward = self.stick.getRawAxis(5)
-        self.motor1.set(forward)
+        forward = self.driver.getRawAxis(5)
+        rotation_value = self.driver.getX(LEFT_HAND)
+        self.drivetrain.arcadeDrive(forward, rotation_value)
+        
         #print(self.encoder.get())
 
+def createTalonAndFollower(MASTER, follower):
+    '''
+    First ID must be MASTER, Second ID must be follower talon
+    This assumes that the left and right sides are the same, two talons.
+    '''
+    master_talon = wpilib.Talon(MASTER)
+    follower_talon = wpilib.Talon(follower)
+    follower_talon.follow(master_talon)
+    return master_talon
 
 if __name__ == "__main__":
 	wpilib.run(Robot)
--- robotmap.py
+++ robotmap.py
@@ -1,16 +1,8 @@
 deadzone = 0.2
 
-# omni = {
-#     'left_motor': 7,
-#     'right_motor': 7,
-#     'front_strafe': 7,
-#     'back_strafe': 3
-# }
-
-
-omni = {
-    'left_motor': 5,
-    'right_motor': 1,
-    'front_strafe': 2,
-    'back_strafe': 3
+robot = {
+    "LEFT_MASTER_ID": 1,
+    "LEFT_FOLLOWER_ID": 2,
+    "RIGHT_MASTER_ID": 3,
+    "RIGHT_FOLLOWER_ID": 4
 }
