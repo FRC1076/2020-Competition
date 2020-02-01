6a65e9e6d9a75ef5484e0c189e1ff5f121ff0ec9
Matthew Jones <mcolinj@mac.com>
2020-02-01 01:46:32 +0000

    made images continuously display, changed image filtering, changed definition of object center, changed color bounds

--- imagetest.py
+++ imagetest.py
@@ -18,17 +18,18 @@ def removeNoise(hsv_img, kernelSize, lower_color_range, upper_color_range):
     # Remove noisy parts of the masked image
     no_noise = cv2.morphologyEx(close_gaps, cv2.MORPH_OPEN, kernel)
     # Undo the erosion to the actual target done during noise removal
-    dilate = cv2.dilate(no_noise, np.ones((5,10), np.uint8), iterations=5)
-    return dilate
+    #dilate = cv2.dilate(no_noise, np.ones((5,10), np.uint8), iterations=5)
+    #return dilate
+    return no_noise
 
 def findObjectContours(dilate, objName):
     # Find boundary of object
     dilcopy = dilate.copy()
     _, contours, _ = cv2.findContours(dilate, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     #print(len(returns))
-    con = contours[0]
+    con = contours[0] #Come back and make sure contours is not empty
     x, y, w, h = cv2.boundingRect(con)
-    obj_center = (int(x+(w/2)), int(y+(h/2))) 
+    obj_center = (int(x+(w/2)), int(y)) 
     img_center = (dilate.shape[1]/2, dilate.shape[0]/2)
     xydist = (obj_center[0]-img_center[0], obj_center[1] -img_center[1])
     print(str(xydist) + "x,y distances")
@@ -40,7 +41,7 @@ def findObjectContours(dilate, objName):
     contoured_image = cv2.rectangle(contoured_image, (x,y), (x+w, y+h), (100, 0, 0), 2)
     contoured_image = cv2.circle(contoured_image, obj_center, 3, (100, 0, 0), 2)
     cv2.imshow("contours", contoured_image)
-    cv2.waitKey(0) #Waits long enough for image to load
+    cv2.waitKey(1) #Waits long enough for image to load
 
     # # Only proceed if contours were found
     # if(contours != None):
@@ -68,37 +69,36 @@ def capture_images(device):
 
     cv2.imwrite("test_images/frame1.jpg", img=frame)
     cv2.imshow("frame", frame)
-    cv2.waitKey(0)
+    cv2.waitKey(1)
     return frame
 if __name__ == "__main__":
-    print("Imagetest main is running")
-    capture_images(0)
-    bgr_img = cv2.imread("test_images/frame1.jpg")
-    # cv2.imshow("Hello", bgr_img)
-    # cv2.waitKey(5000) # keeps the window open for 5 seconds--long enough to load the image
-    # Convert the frame to HSV
-    hsv_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2HSV)
-    #cv2.imshow("Win1", bgr_img)
-
-    #img_hsv_lower = np.array([70, 170, 30])
-    #img_hsv_upper = np.array([90, 200, 50])
-    #color = whiteboard_color = np.array([110,91,80])
-    #color = green_color = np.array([34, 177, 76])
-    #color = retro_green = np.array([])
-    #bound = 40
-    #img_hsv_lower = np.array(color - bound)
-    #img_hsv_upper = np.array(color + bound)
-    img_hsv_lower = np.array([49, 70, 87]) #NOTE: opencv hsv is 0-179, 0-255, 0-255
-    img_hsv_upper = np.array([180, 255, 225])
-    img_dilate = removeNoise(bgr_img, (3,3), img_hsv_lower, img_hsv_upper)
-    cv2.imshow("dilated image", img_dilate)
-    cv2.waitKey(0)
-    #print (hex_dilate.shape)
-    #hex_img = np.array([])
-    findObjectContours(img_dilate, "retroreflective")
     while True:
+        print("Imagetest main is running")
+        capture_images(0)
+        bgr_img = cv2.imread("test_images/frame1.jpg")
+        # cv2.imshow("Hello", bgr_img)
+        # cv2.waitKey(5000) # keeps the window open for 5 seconds--long enough to load the image
+        # Convert the frame to HSV
+        hsv_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2HSV)
+        #cv2.imshow("Win1", bgr_img)
 
-        continue
+        #img_hsv_lower = np.array([70, 170, 30])
+        #img_hsv_upper = np.array([90, 200, 50])
+        #color = whiteboard_color = np.array([110,91,80])
+        #color = green_color = np.array([34, 177, 76])
+        #color = retro_green = np.array([])
+        #bound = 40
+        #img_hsv_lower = np.array(color - bound)
+        #img_hsv_upper = np.array(color + bound)
+        img_hsv_lower = np.array([5, 34, 15]) #NOTE: opencv hsv is 0-179, 0-255, 0-255
+        img_hsv_upper = np.array([100, 255, 225])
+        img_dilate = removeNoise(bgr_img, (1,1), img_hsv_lower, img_hsv_upper)
+        cv2.imshow("dilated image", img_dilate)
+        cv2.waitKey(1)
+        #print (hex_dilate.shape)
+        #hex_img = np.array([])
+        findObjectContours(img_dilate, "retroreflective")
+        
 
     # # # Display the BGR image with found objects bounded by rectangles
     # # if(displayImages):
