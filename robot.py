dfdf50b1668bd8a026ad7c9030db324fd4046e74
William Ballbach <2021ballbachwilliamd@aaps.k12.mi.us>
2020-01-31 20:32:39 -0500

    Added last year's deadzone, streamlined robotmap

--- robot.py
+++ robot.py
@@ -13,8 +13,8 @@ class Robot(wpilib.TimedRobot):
         self.driver = wpilib.XboxController(0)
         self.operator = wpilib.XboxController(1)
 
-        self.left_side = wpilib.SpeedControllerGroup(robotmap.robot["LEFT_LEADER_ID"], robotmap.robot["LEFT_FOLLOWER_ID"])
-        self.right_side = wpilib.SpeedControllerGroup(robotmap.robot["RIGHT_LEADER_ID"], robotmap.robot["RIGHT_FOLLOWER_ID"])
+        self.left_side = wpilib.SpeedControllerGroup(robotmap.LEFT_LEADER_ID, robotmap.LEFT_FOLLOWER_ID)
+        self.right_side = wpilib.SpeedControllerGroup(robotmap.RIGHT_LEADER_ID, robotmap.RIGHT_FOLLOWER_ID)
 
         self.drivetrain = wpilib.drive.DifferentialDrive(self.left_side, self.right_side)
 
@@ -34,11 +34,23 @@ class Robot(wpilib.TimedRobot):
 
     def teleopPeriodic(self):
         forward = self.driver.getRawAxis(5)
+        forward = deadzone(forward, robotmap.deadzone)
+        
         rotation_value = self.driver.getX(LEFT_HAND)
         #TODO: figure out for sure what drive type we're using
         self.drivetrain.arcadeDrive(forward, rotation_value)
         
         #print(self.encoder.get())
+
+def deadzone(val, deadzone):
+    if abs(val) < deadzone:
+        return 0
+    elif val < (0):
+        x = ((abs(val) - deadzone)/(1-deadzone))
+        return (-x)
+    else:
+        x = ((val - deadzone)/(1-deadzone))
+        return (x)
 
 if __name__ == "__main__":
 	wpilib.run(Robot)
--- robotmap.py
+++ robotmap.py
@@ -1,8 +1,7 @@
 deadzone = 0.2
 
-robot = {
-    "LEFT_LEADER_ID": 1,
-    "LEFT_FOLLOWER_ID": 2,
-    "RIGHT_LEADER_ID": 3,
-    "RIGHT_FOLLOWER_ID": 4
-}
+#Motor IDs
+LEFT_LEADER_ID = 1
+LEFT_FOLLOWER_ID = 2
+RIGHT_LEADER_ID = 3
+RIGHT_FOLLOWER_ID = 4
