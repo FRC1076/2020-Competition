2eb05ecdc62d2bda5d8e1e381d71fa03a68cb265
William Ballbach <2021ballbachwilliamd@aaps.k12.mi.us>
2020-02-02 17:28:11 -0500

    Added more comments + info

--- robot.py
+++ robot.py
@@ -1,22 +1,30 @@
 import wpilib
 from wpilib.interfaces import GenericHID
+#TODO: What else will we need for 2020?
+#TODO: Create and import subsystems (shooter, climb, etc.)
 
 #This year, all IDs are stored in the robotmap
 import robotmap
 
+#Controller hands (sides)
 LEFT_HAND = GenericHID.Hand.kLeft
 RIGHT_HAND = GenericHID.Hand.kRight
 
 class Robot(wpilib.TimedRobot):
     def robotInit(self):
-        #DRIVETRAIN
+        #Controllers
         self.driver = wpilib.XboxController(0)
         self.operator = wpilib.XboxController(1)
 
+        #Motors
         self.left_side = wpilib.SpeedControllerGroup(robotmap.LEFT_LEADER_ID, robotmap.LEFT_FOLLOWER_ID)
         self.right_side = wpilib.SpeedControllerGroup(robotmap.RIGHT_LEADER_ID, robotmap.RIGHT_FOLLOWER_ID)
 
+        #Drivetrain
         self.drivetrain = wpilib.drive.DifferentialDrive(self.left_side, self.right_side)
+
+        #TODO: Add subsystems and sensors as the code is written
+        #TODO: SmartDashboard
 
     def robotPeriodic(self):
         return
@@ -28,21 +36,24 @@ class Robot(wpilib.TimedRobot):
         pass
 
     def teleopInit(self):
-        #self.encoder = wpilib.Encoder(0,1)
-        #setup wheel diameter
-        print("Teleop begins!")
+        #TODO: Add encoders, other sensors
+        # print("Teleop begins!")
+        pass
 
     def teleopPeriodic(self):
-        forward = self.driver.getRawAxis(5)
+        forward = self.driver.getRawAxis(5) #Right stick y-axis
         forward = deadzone(forward, robotmap.deadzone)
         
         rotation_value = self.driver.getX(LEFT_HAND)
         #TODO: figure out for sure what drive type we're using
         self.drivetrain.arcadeDrive(forward, rotation_value)
         
-        #print(self.encoder.get())
-
 def deadzone(val, deadzone):
+    """
+    Given the deadzone value x, the deadzone both eliminates all
+    values between -x and x, and scales the remaining values from
+    -1 to 1, to (-1 + x) to (1 - x)
+    """
     if abs(val) < deadzone:
         return 0
     elif val < (0):
@@ -54,4 +65,3 @@ def deadzone(val, deadzone):
 
 if __name__ == "__main__":
 	wpilib.run(Robot)
-    
